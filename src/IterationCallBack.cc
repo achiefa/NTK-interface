2ba66027591007466a1982fe71c9a1551f039ed3
achiefa <amedeochiefa@gmail.com>
2024-05-08 19:40:49 +0200

    Saving dNN and ddNN to file

--- src/IterationCallBack.cc
+++ src/IterationCallBack.cc
@@ -1,5 +1,6 @@
 #include "NTK/AnalyticCostFunction.h"
 #include "NTK/IterationCallBack.h"
+#include "NTK/derive.h"
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -7,6 +8,8 @@
 
 namespace NTK
 {
+  typedef std::vector<std::vector<double>> vec_vec_data;
+
   volatile sig_atomic_t stop;
 
 //_________________________________________________________________________
@@ -73,21 +76,45 @@ IterationCallBack::IterationCallBack(bool VALIDATION,
     const double chi2t_tot = _chi2t->Evaluate();
 
     nnad::FeedForwardNN<double> *NN = _chi2t->GetNN();
+    std::vector<double> parameters = NN->GetParameters();
+
+    // Initialise std::function for second derivative
+    std::function<std::vector<double>(std::vector<double> const&, std::vector<double>)> dNN
+    {
+      [&] (std::vector<double> const& x, std::vector<double> parameters) -> std::vector<double>
+      {
+        nnad::FeedForwardNN<double> aux_nn{*NN}; // Requires pointer dereference
+        aux_nn.SetParameters(parameters);
+        return aux_nn.Derive(x);
+      }
+    };
 
     // Store value as row major order
     int ndata = _chi2t->GetData().size();
     int Size = 4;
     int StepSize = int(ndata / 4);
     nnad::Matrix NTK {Size, Size, std::vector<double> (Size * Size, 0.)};
+    const double eps = 1.e-5;
+    std::vector<std::vector<double>> d_NN;
+    auto *ddNN = new vec_vec_data[Size];
+
+    // Iterate over range of data
+    // TODO
+    // Both vectors for first and second order derivatives contain
+    // spurious terms in the first positions of the arrays. These spurious terms
+    // (non-derived outputs and first derivatives of the ouputs respectively)
+    // should be removed before saving them to data.
     for (int a = 0; a < Size; a++){
+      std::vector<double> input_a = std::get<0>(_chi2t->GetData()[(a) * StepSize]);
+
+      // Compute first and second derivative of the network
+      d_NN.push_back(NN->Derive(input_a));
+      ddNN[a] = NTK::FiniteDifference(dNN, parameters, input_a, eps);
       for (int b = 0; b < Size; b++){
-          std::vector<double> input_a = std::get<0>(_chi2t->GetData()[(a) * StepSize]);
           std::vector<double> input_b = std::get<0>(_chi2t->GetData()[(b) * StepSize]);
           NTK.SetElement(a,b, NN->NTK(input_a, input_b).GetElement(0,0));
       }
     }
-
-    //NTK.Display();
 
     // Output parameters into yaml file
     YAML::Emitter emitter;
@@ -101,6 +128,14 @@ IterationCallBack::IterationCallBack(bool VALIDATION,
       emitter << YAML::Key << "validation chi2" << YAML::Value << chi2v;
     //emitter << YAML::Key << "parameters" << YAML::Value << YAML::Flow << vpar;
     emitter << YAML::Key << "NTK" << YAML::Value << YAML::Flow << NTK.GetVector();
+    emitter << YAML::Key << "dNN" << YAML::BeginMap;
+    for (int ix = 0; ix < Size; ix++)
+      emitter << YAML::Key << "input_" + std::to_string(ix+1) << YAML::Value << YAML::Flow << d_NN[ix];
+    emitter << YAML::EndMap;
+    emitter << YAML::Key << "ddNN" << YAML::BeginMap;
+    for (int ix = 0; ix < Size; ix++)
+      emitter << YAML::Key << "input_" + std::to_string(ix+1) << YAML::Value << YAML::Flow << ddNN[ix];
+    emitter << YAML::EndMap;
     emitter << YAML::EndMap;
     emitter << YAML::EndSeq;
     emitter << YAML::Newline;
